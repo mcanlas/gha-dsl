9b647aea9fdf2872ff28fb2c9c9ba91471246c89
Mark Canlas <github@htmlism.com>
2022-12-26 17:20:22 -0500

    encode nothing

--- src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
+++ src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
@@ -5,6 +5,16 @@ import java.nio.file._
 import scala.jdk.CollectionConverters._
 
 object WriteYaml extends App {
+  val workflow =
+    GitHubActionsWorkflow()
+
   Files
-    .write(Path.of(".github", "workflows", "ci.yml"), Nil.asJava)
+    .write(Path.of(".github", "workflows", "ci.yml"), Encoder.encode(workflow).asJava)
+}
+
+case class GitHubActionsWorkflow()
+
+object Encoder {
+  def encode(wf: GitHubActionsWorkflow): List[String] =
+    Nil
 }
