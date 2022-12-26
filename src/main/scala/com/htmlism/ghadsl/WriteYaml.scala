8d98499c001ed1a535eca7398a85a81d66fc6f5c
Mark Canlas <github@htmlism.com>
2022-12-26 17:41:00 -0500

    try common triggers

--- src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
+++ src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
@@ -3,18 +3,53 @@ package com.htmlism.ghadsl
 import java.nio.file._
 
 import scala.jdk.CollectionConverters._
+import scala.util.chaining._
+
+import cats.data.NonEmptyList
+
+import com.htmlism.ghadsl.GitHubActionsWorkflow.TriggerEvent._
+import com.htmlism.ghadsl.GitHubActionsWorkflow._
+import com.htmlism.ghadsl.LineEncoder._
 
 object WriteYaml extends App {
   val workflow =
-    GitHubActionsWorkflow()
+    GitHubActionsWorkflow(NonEmptyList.of(PullRequest(), Push()))
 
   Files
-    .write(Path.of(".github", "workflows", "ci.yml"), Encoder.encode(workflow).asJava)
+    .write(Path.of(".github", "workflows", "ci.yml"), workflow.encode.asJava)
 }
 
-case class GitHubActionsWorkflow()
+/**
+  * A GHA job will not run without triggers specified
+  */
+case class GitHubActionsWorkflow(triggerEvents: NonEmptyList[TriggerEvent])
 
-object Encoder {
-  def encode(wf: GitHubActionsWorkflow): List[String] =
-    Nil
+object GitHubActionsWorkflow {
+  implicit val ghaEncoder: LineEncoder[GitHubActionsWorkflow] =
+    (wf: GitHubActionsWorkflow) => List("on:") ++ wf.triggerEvents.toList.flatMap(_.encode).pipe(indents)
+
+  sealed trait TriggerEvent
+
+  object TriggerEvent {
+    implicit val triggerEventEncoder: LineEncoder[TriggerEvent] = {
+      case PullRequest() =>
+        List("pull_request:") ++ List("branches: ['**']").pipe(indents)
+
+      case Push() =>
+        List("push:") ++ List("branches: ['**']").pipe(indents)
+    }
+
+    case class PullRequest() extends TriggerEvent
+
+    case class Push() extends TriggerEvent
+  }
+}
+
+trait LineEncoder[A] {
+  def encode(x: A): List[String]
+}
+
+object LineEncoder {
+  def indents(xs: List[String]): List[String] =
+    xs.map("  " + _)
 }
