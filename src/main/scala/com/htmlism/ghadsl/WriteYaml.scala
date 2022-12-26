9c3987104e7468221efa95dba321fb070d4d9a17
Mark Canlas <github@htmlism.com>
2022-12-26 18:10:05 -0500

    with empty steps

--- src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
+++ src/main/scala/com/htmlism/ghadsl/WriteYaml.scala
@@ -15,7 +15,20 @@ object WriteYaml extends App {
   val workflow =
     GitHubActionsWorkflow(
       NonEmptyList.of(PullRequest(), Push()),
-      NonEmptyList.of(Job("foo"), Job("bar"))
+      NonEmptyList.of(
+        Job(
+          "foo",
+          NonEmptyList.of(
+            Job.Step.Runs("echo hello")
+          )
+        ),
+        Job(
+          "bar",
+          NonEmptyList.of(
+            Job.Step.Uses("actions/checkout@v2")
+          )
+        )
+      )
     )
 
   Files
@@ -67,13 +80,32 @@ object GitHubActionsWorkflow {
     case class Push() extends TriggerEvent
   }
 
-  case class Job(name: String)
+  case class Job(name: String, steps: NonEmptyList[Job.Step])
 
   object Job {
     implicit val jobEncoder: LineEncoder[Job] =
       (j: Job) => {
-        List(j.name + ":")
+        val stepsLines =
+          List("steps:") ++ j.steps.toList.flatMap(_.encode.pipe(asArrayElement))
+
+        List(j.name + ":") ++ stepsLines.pipe(indents)
       }
+
+    sealed trait Step
+
+    object Step {
+      implicit val stepEncoder: LineEncoder[Step] = {
+        case Uses(s) =>
+          List("uses: " + s)
+
+        case Runs(s) =>
+          List("run: " + s)
+      }
+
+      case class Uses(s: String) extends Step
+
+      case class Runs(s: String) extends Step
+    }
   }
 }
 
@@ -84,6 +116,15 @@ trait LineEncoder[A] {
 object LineEncoder {
   def indents(xs: List[String]): List[String] =
     xs.map("  " + _)
+
+  def asArrayElement(xs: List[String]): List[String] =
+    xs match {
+      case head :: tail =>
+        ("  - " + head) +: tail.map("    " + _)
+
+      case Nil =>
+        Nil
+    }
 
   def interConcat[A](join: List[A])(xxs: List[List[A]]): List[A] =
     xxs match {
