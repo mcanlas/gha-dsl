418533447d827a13587e02f89e5b1dbe4177a7b7
Mark Canlas <github@htmlism.com>
2026-03-13 21:31:21 -0400

    dependencies :abacus:

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,27 +23,5 @@ object ProjectPlugin extends AutoPlugin {
     def module(s: String): Project =
       Project(s, file(jarName(s)))
         .settings(name := jarName(s))
-
-    implicit class ProjectOps(p: Project) {
-      def withCats: Project =
-        p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
-
-      def withYaml: Project =
-        p
-          .settings(libraryDependencies += "io.circe" %% "circe-yaml" % "0.15.1")
-
-      def withTesting: Project = {
-        val weaverVersion =
-          "0.8.4"
-
-        p.settings(
-          libraryDependencies ++= Seq(
-            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
-            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
-          )
-        )
-      }
-    }
   }
 }
